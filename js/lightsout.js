d30be08c3279a34219dec02c8769b4b9f2289759
Ken Wilder <kjwilder@gmail.com>
2021-05-24 20:30:11 -0400

    Add Javascript template (#3)

---
+++ js/lightsout.js
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
+    <title>Lights Out!</title>
+  </head>
+  <body>
+    <h1>Lights Out!</h1>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+  </body>
+</html>
+
